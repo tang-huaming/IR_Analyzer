a79a6e9600f8e2a1a23cc67ad4342c2492f277fd
tang-huaming <xiaomanon@163.com>
2016-03-06 22:47:48 +0800

    添加安装调试对话框DEMO
    添加安装调试对话框DEMO，目前功能还未完善

---
+++ demo/SetupDebug/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle(tr("安装调试对话框"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
